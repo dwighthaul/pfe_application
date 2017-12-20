bdf9a545df213395d30e1ce4b40362850d924736
Morgan Cocherel <morgan.cocherel@gmail.com>
2017-12-20 15:13:56 +0100

    Test Commit

--- app/src/main/java/fr/eseo/dis/hubertpa/pfe_application/ListProject.java
+++ app/src/main/java/fr/eseo/dis/hubertpa/pfe_application/ListProject.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class ListProject extends AppCompatActivity {
-
+	//ceci est un test de commit
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
