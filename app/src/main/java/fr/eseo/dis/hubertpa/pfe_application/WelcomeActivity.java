40c86797809d98715c4cb9abbc07ebce39a1f50b
paulhubert <27111994.Phgit>
2017-12-20 14:55:21 +0100

    Initial commit

---
+++ app/src/main/java/fr/eseo/dis/hubertpa/pfe_application/WelcomeActivity.java
@@ -0,0 +1,57 @@
+package fr.eseo.dis.hubertpa.pfe_application;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+	Button visiteur;
+	Button login;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_welcome);
+
+		login = (Button) findViewById(R.id.button_login);
+		visiteur = (Button) findViewById(R.id.button_visitor);
+
+		login.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View view) {
+				Log.d("Welcome", "Connect");
+				LoginActivity();
+			}
+
+		});
+
+		visiteur.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View view) {
+				Log.d("Welcome", "Visiteur");
+				VisitorActivity();
+			}
+		});
+
+	}
+
+	public void LoginActivity() {
+		Intent intent = new Intent(this, LoginActivity.class);
+		startActivity(intent);
+	}
+
+	public void VisitorActivity() {
+		//VisitorActivity
+		Intent intent = new Intent(this, LoginActivity.class);
+		startActivity(intent);
+	}
+
+}
