ae303fa98bff0516058191177420f4d47bb249a0
Morgan Cocherel <morgan.cocherel@gmail.com>
2017-12-30 17:59:40 +0100

    Prepare some features that we'll need : bottom navigation and buttons

--- app/src/main/java/fr/eseo/dis/hubertpa/pfe_application/ListProject.java
+++ app/src/main/java/fr/eseo/dis/hubertpa/pfe_application/ListProject.java
@@ -3,11 +3,16 @@ package fr.eseo.dis.hubertpa.pfe_application;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import fr.eseo.dis.hubertpa.pfe_application.partials.NavigationBottom;
+
 public class ListProject extends AppCompatActivity {
-	//ceci est un test de commit
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_list_project);
+
+		NavigationBottom navigationBottom = new NavigationBottom();
 	}
+
+
 }
---
+++ app/src/main/java/fr/eseo/dis/hubertpa/pfe_application/partials/NavigationBottom.java
@@ -0,0 +1,33 @@
+package fr.eseo.dis.hubertpa.pfe_application.partials;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+
+import fr.eseo.dis.hubertpa.pfe_application.R;
+
+/**
+ * Created by Morgan on 30/12/2017.
+ */
+
+public class NavigationBottom {
+    /*findViewById(R.id.bottom_navigation).setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            switch (item.getItemId()){
+
+                case R.id.action_project_tab:
+                    //Action quand onglet 1 sélectionné
+                    break;
+                case R.id.action_jury_tab:
+                    //Action quand onglet 2 sélectionné
+                    break;
+
+                default:
+                    //Action quand onglet 3 sélectionné
+
+                    break;
+            }
+        });*/
+}
